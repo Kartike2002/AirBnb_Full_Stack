da92447c7377c6d3b4fa946b87151549552ae75a
Kartike2002 <adityanigam14569@gmail.com>
2026-01-19 16:57:15 +0530

    Updated

--- app.js
+++ app.js
@@ -1,71 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const listing = require('./models/listing.js');
+const mongoose = require("mongoose");
 const path = require("path");
 
+// Import Model (ONLY ONCE)
+const Listing = require("./models/listing");
 
-const mongoURI = 'mongodb://localhost:27017/mydatabase';
+// MongoDB Connection
+const mongoURI = "mongodb://localhost:27017/mydatabase";
 
-main()
-.then(()=>{
-    console.log('Connected to MongoDB');
-
-})
-.catch((err) =>{
-    console.log(err);
-});
-
-
-
-async function main(){
+async function main() {
     await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB");
 }
 
+main().catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
 
-app.set("view engine" , "ejs");
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({extended:true}));
-
 // Index Route
-app.get('/listings', async (req, res) => {
-    const allListings = await listing.find({});
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 });
 
 
-//Show Route
+// New Route
+app.get("/listings/new", async (req, res) => {
+    res.render("listings/new.ejs");
+});
 
-app.get('/listings/:id', async (req, res) => {
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
 });
 
- 
+//Create Route
 
-
-
-
-// app.get('/testListing' , async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: "Sample Listing",
-//         description: "This is a sample listing.",
-//         image: "default link",
-//         price: 100,
-//         location: "Sample Location",
-//         country: "Sample Country"
-//     });
-//     await sampleListing.save();
-//     console.log('Sample listing saved to database');
-//     res.send(sampleListing);
-// });
-
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+app.post("/listings", async (req, res) => {
+    const listingData = req.body.listing;
+    await Listing.create(listingData);
+    res.redirect("/listings");
 });
 
+
+
+// Server
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
